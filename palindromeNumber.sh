fc015a043ca1ec458a8f7caf9ebe169ba92324fc
Aditya <adi.dubey2912@gmail.com>
2022-08-17 22:38:59 +0530

    Find a palindrome number.

---
+++ palindromeNumber.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+read -p "Enter the number: " num
+
+temp=$num
+while (($num > 0))
+do
+sum=$(($num%10))
+num=$(($num/10))
+rev=$( echo ${rev}${sum} )
+done
+
+if (($temp == $rev))
+then
+echo "$temp is a palindrome number."
+else
+echo "$temp is not a palindrome number."
+fi
