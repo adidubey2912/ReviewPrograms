3c7489b88dd8aa7c1c3e1aa13676395adf89dc0f
Aditya <adi.dubey2912@gmail.com>
2022-08-18 17:36:48 +0530

    Check a prime number

--- primeNumber.sh
+++ primeNumber.sh
@@ -2,10 +2,28 @@
 
 read -p "Enter the number: " num
 
-if (($num%2 == 0))
-then
-echo "$num is not a prime number."
-else
-echo "$num is a prime number."
-fi
+function primeNumber(){
+	if (( $num == 1 ))
+	then
+		echo "$num is not a prime number."
+	elif (( $num == 2 ))
+	then
+		echo "$num is a prime number."
+	else
+		for (( x=2; x < $num; x++ ))
+		do
+			if (( $num % $x == 0 ))
+			then
+				result="not"
+			fi
+		done
+		if [ "$result" == "not" ]
+		then
+			echo "$num is not a prime number."
+		else
+			echo "$num is a prime number."
+		fi
+	fi
+}
 
+primeNumber
